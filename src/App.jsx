cfd55c3f128270db5f6a2e4b47b37f4ab3092129
SSabu <sandeepsabu09@gmail.com>
2017-07-31 15:09:40 -0700

    Adding fixed component for Git Issues

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Login from './components/Login/Login';
 import Organizations from './components/Organizations/Organizations';
 import Dashboard from './components/Dashboard/Dashboard';
 import './App.css';
+import Issues from './components/Issues/Issues.jsx';
 
 const App = ({ currentPage }) => (
   <Container>
