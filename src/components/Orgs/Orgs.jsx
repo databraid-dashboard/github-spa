fa5c0c67d60d22ef3c8b36c76997e8659bf1aaca
SSabu <sandeepsabu09@gmail.com>
2017-08-03 09:16:01 -0700

    Add Org component partially built out

---
+++ src/components/Orgs/Orgs.jsx
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+import { Image } from 'semantic-ui-react';
+import Org from '../Org/Org.jsx';
+
+class Organizations extends Components{
+  <Image.Group size='small'>
+    <Org />
+  </Image.Group>
+}
