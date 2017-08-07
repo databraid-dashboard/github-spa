304072bd11151ef63ae2ae13e12801f2707a6284
Grant Willison <emailgdw@gmail.com>
2017-08-07 11:58:26 -0700

    added dashbaord test file

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,14 +1,17 @@
-// import React from 'react';
-// import toJson, { shallowToJson } from 'enzyme-to-json';
-// import { shallow, render, mount } from 'enzyme';
-// import App from './App.jsx';
-//
-//
-// describe('App component', () => {
-//   it('should render a component with props as specified ', () => {
-//     const component = shallow(
-//       <App   />,
-//     );
-//     expect(toJson(component)).toMatchSnapshot();
-//   });
-// })
+import React from 'react';
+import toJson, { shallowToJson } from 'enzyme-to-json';
+import { shallow, render, mount } from 'enzyme';
+import App from './App.jsx';
+
+
+describe('App component', () => {
+  it('should render a component with props as specified ', () => {
+    const component = shallow(
+      <Provider>
+
+        <App   />
+      </Provider>
+    );
+    expect(toJson(component)).toMatchSnapshot();
+  });
+})
