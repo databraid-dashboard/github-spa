e6ac89f9ef0761810dfd0577f37a2afa6d5fb47c
Grant Willison <emailgdw@gmail.com>
2017-08-09 16:50:32 -0700

    Working thorugh Oauth login addition, currently broken

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -6,12 +6,14 @@ import { bindActionCreators } from 'redux';
 import PropTypes from 'prop-types';
 import { renderOrgs } from '../../actions/renderActions';
 import './Login.css';
+const AUTH_ROUTE = 'http://localhost:3000/auth/github';
 
 export const Login = ({ currentPage, renderOrgs }) => {
   if (currentPage !== 'login') {
     return <div />;
   }
   if (currentPage === 'login') {
+
     return (
       <div>
         <Button onClick={() => renderOrgs()}>
@@ -21,6 +23,12 @@ export const Login = ({ currentPage, renderOrgs }) => {
     );
   }
 };
+
+hitAuthRoute( AUTH_ROUTE) => {
+  fetch(AUTH_ROUTE)
+    .then(response =>
+    fetch())
+}
 
 Login.propTypes = {
   currentPage: PropTypes.string.isRequired,
