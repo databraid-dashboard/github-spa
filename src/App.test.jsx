6d135dff395ab30eef52b03fbd4c13bc32a4aa3d
Grant Willison <emailgdw@gmail.com>
2017-08-11 11:43:48 -0700

    fix app test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,14 +1,35 @@
 /* eslint-disable import/extensions */
-
+import promiseMiddleware from 'redux-promise-middleware';
 import React from 'react';
 import toJson from 'enzyme-to-json';
 import { shallow } from 'enzyme';
 import App from './App.jsx';
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import Api from './utils/Api';
+import thunkMiddleware from 'redux-thunk';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers/index';
 
-xdescribe('App component', () => {
+const middleware = [
+  promiseMiddleware(),
+  thunkMiddleware.withExtraArgument({ Api}),
+  thunk,
+];
+const mockStore = configureStore(middleware);
+describe('App component', () => {
   it('should render a component with props as specified ', () => {
     const component = shallow(
-      <App />,
+      <Provider>
+
+        <App   />
+      </Provider>
+    );
+    expect(toJson(component)).toMatchSnapshot();
+  });
+  it('should render a component with props as specified ', () => {
+    const component = shallow(
+      <App store={mockStore}/>,
     );
     expect(toJson(component)).toMatchSnapshot();
   });
